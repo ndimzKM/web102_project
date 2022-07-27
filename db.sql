a7e4a95a191623d50a537312335f5613fe6073ae
sawaneh <sawaneh@pop-os.localdomain>
2022-07-27 22:32:33 +0000

    test111

--- db.sql
+++ db.sql
@@ -3,29 +3,30 @@ CREATE DATABASE bantaba;
 use bantaba;
 
 CREATE TABLE `users` (
-    PRIMARY KEY (`id`)
     `id` INT NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(100) NOT NULL,
     `email` VARCHAR(100) NOT NULL,
     `password` varchar(255) NOT NULL,
     `username` varchar(100) NOT NULL,
+    PRIMARY KEY (`id`)
 );
 
 CREATE TABLE `questions` (
-    PRIMARY KEY (`id`),
+   
     `id` INT NOT NULL AUTO_INCREMENT,
     `userID` INT NOT NULL,
     `question` TEXT NOT NULL,
     `createdAt` TIMESTAMP NOT NULL,
-    FOREIGN KEY (`userID`) REFERENCES users(`id`)
+    FOREIGN KEY (`userID`) REFERENCES users(`id`),
+     PRIMARY KEY (`id`)
 );
 
 CREATE TABLE `answers` (
-    PRIMARY KEY (`id`),
     `id` INT NOT NULL AUTO_INCREMENT,
     `questionID` INT NOT NULL,
     `userID` INT NOT NULL,
     `createdAt` TIMESTAMP NOT NULL,
-    FOREIGN KEY ('questionID') REFERENCES questions(`id`),
-    FOREIGN KEY (`userID`) REFERENCES users(`id`)
+    FOREIGN KEY (`userID`) REFERENCES users(`id`),
+    FOREIGN KEY (`questionID`) REFERENCES questions(`id`),
+    PRIMARY KEY (`id`)
 );
